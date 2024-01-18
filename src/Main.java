3a7289616be805cc33d7ae21c42b9043f6149777
Roman Kornoukhov <kornoukhov.omsk@yandex.ru>
2024-01-18 17:10:37 +0600

    Выполнено задание

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,6 @@
+import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
-import java.awt.geom.*;
-import javax.swing.*;
-import javax.swing.border.Border;
 
 public class Main {
     public static void main(String[] args) {
@@ -12,7 +10,6 @@ public class Main {
         JFrame fr = new JFrame("Движение точки");
         fr.setPreferredSize(new Dimension(WIDTH, HEIGHT));
         final JPanel pan = new JPanel();
-        final JPanel buttonPan = new JPanel();
         JButton increaseButton = new JButton("Увеличить");
         JButton decreaseButton = new JButton("Уменьшить");
         pan.add(increaseButton);
@@ -34,15 +31,15 @@ public class Main {
                 int diameter = 20;
                 gr.translate(x, y);
                 // Рисование круга
-                gr.fillOval(-diameter/2, -diameter/2, diameter, diameter);
+                gr.fillOval(-diameter / 2, -diameter / 2, diameter, diameter);
                 // Изменение координат
                 x = x + speed[0];
                 y = y + speed[1];
                 // Проверка выхода за границы
-                if (!(x > diameter/2 && x < pan.getWidth() - diameter/2)) {
+                if (!(x > diameter / 2 && x < pan.getWidth() - diameter / 2)) {
                     speed[0] = -speed[0];
                 }
-                if (!(y > diameter/2 && y < pan.getHeight() - diameter/2)) {
+                if (!(y > diameter / 2 && y < pan.getHeight() - diameter / 2)) {
                     speed[1] = -speed[1];
                 }
 
@@ -51,25 +48,17 @@ public class Main {
         tm.start();
         // Обработчик события кнопки увелечения
         increaseButton.addActionListener(e -> {
-            if (speed[0] > 0)
-                speed[0] += 2;
-            else
-                speed[0] -= 2;
-            if (speed[1] > 0)
-                speed[1] += 2;
-            else
-                speed[1] -= 2;
+            if (speed[0] > 0) speed[0] += 2;
+            else speed[0] -= 2;
+            if (speed[1] > 0) speed[1] += 2;
+            else speed[1] -= 2;
         });
         // Обрабочик события кнопки уменьшения
         decreaseButton.addActionListener(e -> {
-            if (speed[0] > 0)
-                speed[0] -= 2;
-            else
-                speed[0] += 2;
-            if (speed[1] > 0)
-                speed[1] -= 2;
-            else
-                speed[1] += 2;
+            if (speed[0] > 0) speed[0] -= 2;
+            else speed[0] += 2;
+            if (speed[1] > 0) speed[1] -= 2;
+            else speed[1] += 2;
         });
     }
 }
